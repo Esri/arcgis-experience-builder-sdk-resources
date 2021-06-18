c21b6a491468ac0c998f8bf6076784ce35092d26
Junshan Liu <junshan_liu@esri.com>
2021-06-19 01:40:19 +0800

    add unit tests widget (#81)
    * add show ut widget
    
    * add read me
    
    * widget rename
    
    * Update name in manifest.json, widget.tsx, and widget.test.tsx. For https://github.com/Esri/arcgis-experience-builder-sdk-resources/pull/81.
    
    * add unit tests readme
    
    * update title
    
    Co-authored-by: shawnmgoulet <3179974+shawnmgoulet@users.noreply.github.com>
    Co-authored-by: Gavin Rehkemper <grehkemper@esri.com>

---
+++ widgets/show-unit-tests/tests/widget.test.tsx
@@ -0,0 +1,74 @@
+import { React, getAppStore, appActions } from 'jimu-core';
+import _Widget from '../src/runtime/widget';
+import { widgetRender, wrapWidget, getInitState, getDefaultAppConfig } from 'jimu-for-test'
+import { fireEvent } from '@testing-library/react'
+
+const render = widgetRender();
+const Widget = wrapWidget(_Widget, { config: {} });
+
+jest.mock('jimu-core', () => {
+  return {
+    ...jest.requireActual('jimu-core'),
+    loadArcGISJSAPIModule: jest.fn().mockImplementation(moduleId => {
+      let module
+      if (moduleId === 'esri/layers/FeatureLayer') {
+        module = jest.fn().mockImplementation(() => {
+          return {
+            queryFeatureCount: () => Promise.resolve(5)
+          }
+        })
+      }
+      return Promise.resolve(module)
+    })
+  }
+})
+
+describe('test show-unit-tests widget', () => {
+  it('test p1, use render props', () => {
+    const { queryByText, rerender } = render(<Widget config={{ p1: true }} />);
+    expect(queryByText('p1 is checked.').className).toBe('has-p1');
+
+    rerender(<Widget config={{ p1: false }} />);
+    expect(queryByText('p1 is checked.')).toBeNull();
+  })
+
+  it('test label, map from state', () => {
+    getAppStore().dispatch(appActions.updateStoreState(getInitState().merge({
+      appConfig: getDefaultAppConfig().merge({
+        widgets: {
+          w1: {
+            label: 'W 1'
+          }
+        }
+      })
+    })))
+
+    const renderResult = render(<Widget widgetId="w1" />);
+    expect(renderResult.queryByText('Widget label:W 1')).toBeInTheDocument();
+  })
+
+  it('test theme variable', () => {
+    getAppStore().dispatch(appActions.updateStoreState(getInitState().merge({
+      theme: {
+        colors: {
+          black: '#FFF'
+        }
+      }
+    })))
+
+    const renderResult = render(<Widget />);
+    expect(renderResult.queryByText('Theme variable:#FFF')).toBeInTheDocument();
+  })
+
+  it('test event', () => {
+    const { queryByText, queryByRole } = render(<Widget />);
+    fireEvent.click(queryByRole('button', { name: 'Click me' }))
+    expect(queryByText('Count:1')).toBeInTheDocument();
+  })
+
+  it('test api', async () => {
+    const { findByText, queryByRole } = render(<Widget />);
+    fireEvent.click(queryByRole('button', { name: 'Query features' }))
+    await findByText('Query Result count:5');
+  })
+});
