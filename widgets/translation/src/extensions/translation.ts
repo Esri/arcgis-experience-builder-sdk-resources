dce11911bb137e723948f4ea3b59b32177c99ea2
Junshan Liu <junshan_liu@esri.com>
2021-06-23 01:35:26 +0800

    translation widget (#83)
    * add translation widget
    
    * do not replace when run in builer
    
    * update wording
    
    Co-authored-by: Gavin Rehkemper <grehkemper@esri.com>

---
+++ widgets/translation/src/extensions/translation.ts
@@ -0,0 +1,23 @@
+import { extensionSpec, AppConfig, utils, createIntl, getAppStore } from 'jimu-core'
+import defaultMessage from '../runtime/translations/default'
+
+export default class Translation implements extensionSpec.AppConfigProcessorExtension {
+  id = 'translation'
+  widgetId: string
+
+  async process (appConfig: AppConfig): Promise<AppConfig> {
+    // Do not replace when run in builder.
+    if(window.jimuConfig.isInBuilder){
+      return Promise.resolve(appConfig)
+    }
+    const widgetJson = appConfig.widgets[this.widgetId]
+
+    const intl = createIntl({
+      locale: getAppStore().getState().appContext.locale,
+      messages: Object.assign({}, defaultMessage, widgetJson.manifest.i18nMessages)
+    })
+
+    utils.replaceI18nPlaceholdersInObject(appConfig, intl, defaultMessage)
+    return Promise.resolve(appConfig)
+  }
+}
---
+++ widgets/translation/src/runtime/translations/default.ts
@@ -0,0 +1,4 @@
+export default {
+  key1: 'This is label 1',
+  key2: 'This is label 2'
+}
