6b750ba667da6466f0ee756c686a6b9640b63bdd
Junshan Liu <junshan_liu@esri.com>
2021-06-22 21:53:46 +0800

    Update widgets/use-assets/src/runtime/widget.tsx
    Co-authored-by: Shawn Goulet <shawn.goulet@gmail.com>

--- widgets/use-assets/src/runtime/widget.tsx
+++ widgets/use-assets/src/runtime/widget.tsx
@@ -36,7 +36,7 @@ export default function(props: AllWidgetProps<Config>){
           </div>
 
           <div className="m-2">
-            <h5>3.3. Use <b>require</b> to load .svg file and use <b>Icon</b> component to render, The .svg file is inlined and is rendered as image</h5>
+            <h5>3.3. Use <b>require</b> to load a SVG file and use an <b><code>Icon</code></b> component to render. The SVG file is inlined on compile and is rendered as image.</h5>
             <Icon icon={svg2} color="red" width="200px" height="200px"/>
           </div>
 
