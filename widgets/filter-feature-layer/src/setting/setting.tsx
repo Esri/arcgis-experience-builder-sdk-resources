88f3f0bc5d179ba41a5fea6403184f6f23d35395
Gavin Rehkemper <grehkemper@esri.com>
2021-09-14 10:47:33 -0500

    filter-feature-layer sample - use FieldSelector

--- widgets/filter-feature-layer/src/setting/setting.tsx
+++ widgets/filter-feature-layer/src/setting/setting.tsx
@@ -18,10 +18,9 @@
   A copy of the license is available in the repository's
   LICENSE file.
 */
-import { jsx, Immutable, UseDataSource, DataSourceManager, FeatureLayerDataSource, React, FormattedMessage } from "jimu-core";
+import { jsx, Immutable, UseDataSource, JimuFieldType } from "jimu-core";
 import { AllWidgetSettingProps } from "jimu-for-builder";
-import { Select, Option } from "jimu-ui";
-import { DataSourceSelector, AllDataSourceTypes } from "jimu-ui/advanced/data-source-selector";
+import { DataSourceSelector, AllDataSourceTypes, FieldSelector } from "jimu-ui/advanced/data-source-selector";
 import {
   SettingSection,
   SettingRow
@@ -29,35 +28,10 @@ import {
 import { IMConfig } from "../config";
 import defaultI18nMessages from "./translations/default";
 
-const { useState, useEffect } = React;
-
 export default function (props: AllWidgetSettingProps<IMConfig>) {
 
   const supportedDsTypes = Immutable([AllDataSourceTypes.FeatureLayer]);
-
-  const [fields, setFields] = useState([]);
-
-  // When the dataSource is changed, set the fields dropdown values.
-  useEffect(() => {
-    if (props.useDataSources && props.useDataSources.length === 1) {
-
-      const dsManager = DataSourceManager.getInstance();
-      const useDataSource = props.useDataSources[0];
-      const ds: FeatureLayerDataSource = dsManager.getDataSource(useDataSource.mainDataSourceId) as FeatureLayerDataSource;
-
-      const layerDefinition = ds.getLayerDefinition();
-      const fields = layerDefinition.fields.filter((fieldInfo) => {
-        return fieldInfo.type === 'esriFieldTypeString';
-      });
-      setFields(fields);
-    } else {
-      setFields([]);
-      props.onSettingChange({
-        id: props.id,
-        config: props.config.set('filterField', '')
-      });
-    }
-  }, [props.useDataSources]);
+  const supportedFieldTypes = Immutable([JimuFieldType.String]);
 
   // When the dataSource is chosen, save it to the settings.
   const onDataSourceChange = (useDataSources: UseDataSource[]) => {
@@ -73,10 +47,12 @@ export default function (props: AllWidgetSettingProps<IMConfig>) {
 
   // When a field is chosen from the dropdown, save it to the settings.
   const fieldsListChangeHandler = (evt) => {
-    props.onSettingChange({
-      id: props.id,
-      config: props.config.set('filterField', evt.target.value)
-    });
+    if (evt && evt.length === 1) {
+      props.onSettingChange({
+        id: props.id,
+        config: props.config.set('filterField', evt[0].name)
+      });
+    }
   }
 
   return (
@@ -111,11 +87,16 @@ export default function (props: AllWidgetSettingProps<IMConfig>) {
             })}
           >
             <SettingRow>
-              <Select onChange={(e) => { fieldsListChangeHandler(e) }} value={props.config.filterField}>
-                {fields.map((fieldInfo) => {
-                  return <Option value={fieldInfo.name}>{fieldInfo.alias}</Option>;
-                })}
-              </Select>
+              <FieldSelector
+                useDataSources={props.useDataSources}
+                types={supportedFieldTypes}
+                onChange={fieldsListChangeHandler}
+                selectedFields={Immutable([props.config.filterField])}
+                isMultiple={false}
+                isSearchInputHidden={false}
+                isDataSourceDropDownHidden
+                useDropdown
+              />
             </SettingRow>
           </SettingSection>
         }
