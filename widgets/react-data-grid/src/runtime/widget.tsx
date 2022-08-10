9dd2468740df21d6cd8604f6ddaea3391ebbc783
Gavin Rehkemper <grehkemper@esri.com>
2022-08-10 09:43:52 -0500

    updated the react-data-grid sample to react-data-grid v7beta

--- widgets/react-data-grid/src/runtime/widget.tsx
+++ widgets/react-data-grid/src/runtime/widget.tsx
@@ -18,24 +18,23 @@
   A copy of the license is available in the repository's
   LICENSE file.
 */
-import { React, AllWidgetProps, jsx } from "jimu-core";
-import * as ReactDataGrid from "react-data-grid";
+import { React, AllWidgetProps, jsx } from 'jimu-core'
+import DataGrid from 'react-data-grid'
 
 export default class Widget extends React.PureComponent<AllWidgetProps<any>, any> {
-  private columns = [
-    { key: "id", name: "ID" },
-    { key: "title", name: "Title" },
-    { key: "count", name: "Count" },
+  private readonly columns = [
+    { key: 'id', name: 'ID' },
+    { key: 'title', name: 'Title' }
   ];
 
-  private rows = [
-    { id: 0, title: "row1", count: 20 },
-    { id: 1, title: "row1", count: 40 },
-    { id: 2, title: "row1", count: 60 },
+  private readonly rows = [
+    { id: 0, title: 'Example' },
+    { id: 1, title: 'Demo' }
   ];
+
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {}
   }
 
   render() {
@@ -43,14 +42,9 @@ export default class Widget extends React.PureComponent<AllWidgetProps<any>, any
       <div className="widget-d3 jimu-widget p-2">
         <p>React Data Grid sample</p>
         <p>
-          <ReactDataGrid
-            columns={this.columns}
-            rowGetter={(i) => this.rows[i]}
-            rowsCount={3}
-            minHeight={150}
-          />
+          <DataGrid columns={this.columns} rows={this.rows} />;
         </p>
       </div>
-    );
+    )
   }
 }
