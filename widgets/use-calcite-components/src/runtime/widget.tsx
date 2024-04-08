882844704665be58c4f569a273a2f1496f7f378d
Junshan Liu <junshan_liu@esri.com>
2024-04-08 15:32:04 +0800

    update use calcite widget

--- widgets/use-calcite-components/src/runtime/widget.tsx
+++ widgets/use-calcite-components/src/runtime/widget.tsx
@@ -1,10 +1,5 @@
 import { React, type AllWidgetProps } from 'jimu-core'
 import { type IMConfig } from '../config'
-/**
- * Use this way, the @esri/calcite-components-react code will be compiled into widget entry, which makes widget size large.
- *    `import { CalciteButton, CalciteIcon, CalciteSlider } from '@esri/calcite-components-react'`
- * So, we should use the below way.
- */
 import { CalciteButton, CalciteIcon, CalciteSlider } from 'calcite-components'
 
 const Widget = (props: AllWidgetProps<IMConfig>) => {
