ea2fb2d65a926ab36801dce4564339920bef666c
Niklas Köhn <n.koehn@esri.de>
2024-12-05 12:03:16 +0100

    shared chunks: UI output updated

--- widgets/share-code-chunk/widget1/src/runtime/widget.tsx
+++ widgets/share-code-chunk/widget1/src/runtime/widget.tsx
@@ -11,7 +11,7 @@ export default function Widget (props: AllWidgetProps<unknown>) {
   return (
     <div className="widget-demo jimu-widget m-2">
       <div>Widget 1</div>
-      {myModule && <div>Module loaded, {myModule.f1()}</div>}
+      {myModule && <div>Module loaded.<br/>{myModule.f1('chunky widget 1')}</div>}
     </div>
   )
 }
